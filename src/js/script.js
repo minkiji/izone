214cfb0b5c1af3947c48e864323901416a839853
Arra <145094087+minkiji@users.noreply.github.com>
2024-11-18 04:35:20 +0800

    iz

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,31 @@
 function toggleReadMore(event) {
-    event.preventDefault(); // Prevent the default anchor behavior
-    const moreText = document.querySelector(".more-text");
-    const readMoreLink = event.target.closest("a"); // Use closest to ensure we get the link element
-    const icon = readMoreLink.querySelector("i"); // Get the icon element
-  
-    if (moreText.style.display === "none" || !moreText.style.display) {
-      moreText.style.display = "block"; // Show the extra text
-      readMoreLink.innerHTML = 'Less <i class="fa-solid fa-angle-up"></i>'; // Change link text and icon
-    } else {
-      moreText.style.display = "none"; // Hide the extra text
-      readMoreLink.innerHTML = 'More <i class="fa-solid fa-angle-down"></i>'; // Change link text and icon
-    }
+  event.preventDefault();
+
+  const readMoreLink = event.target.closest("a");
+  const description = readMoreLink.closest(".description") || readMoreLink.closest(".maknae-description");
+  const moreText = description.querySelector(".more-text");
+  const icon = readMoreLink.querySelector("i");
+
+  if (moreText.style.display === "none" || !moreText.style.display) {
+    moreText.style.display = "block";
+    readMoreLink.innerHTML = 'Less <i class="fa-solid fa-angle-up"></i>';
+  } else {
+    moreText.style.display = "none";
+    readMoreLink.innerHTML = 'More <i class="fa-solid fa-angle-down"></i>';
   }
+}
+
+
+window.onload = () => {
+  document.querySelectorAll(".card-container").forEach((parent) => {
+    parent.querySelectorAll(".card").forEach((container) => {
+      container.addEventListener("mouseover", () => {
+        parent.classList.add("children-focused");
+      });
   
+      container.addEventListener("mouseleave", () => {
+        parent.classList.remove("children-focused");
+      });
+    });
+  });
+};
